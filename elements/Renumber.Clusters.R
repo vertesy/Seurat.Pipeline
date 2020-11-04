5057c6554bd82cf8b665660f7e3fbdb00cea6d2a
Abel Vertesy <vertesy@users.noreply.github.com>
2020-11-04 14:04:07 +0100

    renumber clusters

---
+++ elements/Renumber.Clusters.R
@@ -0,0 +1,46 @@
+######################################################################
+# Renumber.Clusters.R
+######################################################################
+# source ("~/GitHub/Packages/Seurat.pipeline/elements/Renumber.Clusters.R")
+
+
+# Functions ------------------------
+source ('~/GitHub/Packages/Seurat.utils/Cluster.Auto-naming.DE.R')
+stopifnot(exists('p'))
+stopif(is.null(p$'OrderClsByPrCurve'),message = 'OrderClsByPrCurve not found')
+stopif(is.null(p$'res.analyzed.DE'),message = 'res.analyzed.DE not found')
+
+if (p$'OrderClsByPrCurve') require(princurve)
+
+# Setup ------------------------
+create_set_Original_OutDir()
+suffix =""
+prefix ="integrated_snn_res"
+
+
+# AutoNumber.by.UMAP ------------------------
+for (i in 1:length(p$'res.analyzed.DE')) {
+  res = p$'res.analyzed.DE'[i]
+  (res.full = trimws(ppp(prefix, res, suffix), whitespace = '\\.'))
+
+  combined.obj <- AutoNumber.by.UMAP(obj = combined.obj, dimension=1, reduction="umap", swap = F, res = res.full )
+  identity.used <- ppp(res.full, "ordered")
+  umap.snn_res.X.ordered <- DimPlot.ClusterNames(ident = identity.used)
+  qqsave(umap.snn_res.X.ordered
+         , plotname = ppp("umap",identity.used), PNG = T, plotit=T)
+
+
+  # AutoNumber.by.PrinCurve ------------------------
+  if (p$'OrderClsByPrCurve') {
+    combined.obj <- AutoNumber.by.PrinCurve(obj = combined.obj, dimension=1:2, reduction="umap", plotit=T, swap= -1, res = res.full )
+    identity.used <- ppp(res.full, "prin.curve")
+    umap.snn_res.X.prin.curve <- DimPlot.ClusterNames(ident = identity.used)
+    qqsave(umap.snn_res.X.prin.curve
+           , plotname = ppp("umap",identity.used), PNG = T, plotit=T)
+  }
+
+
+}
+
+
+
