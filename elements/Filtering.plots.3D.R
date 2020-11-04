ac99d4d94cea8e084b89dada5f175aeb727e3f06
Abel Vertesy <vertesy@users.noreply.github.com>
2020-11-04 14:18:39 +0100

    filtering plot to loop around

--- elements/Filtering.plots.3D.R
+++ elements/Filtering.plots.3D.R
@@ -5,55 +5,66 @@
 # try(dev.off(), silent = T)
 
 # Setup ------------------------
-create_set_OutDir(OutDirOrig, "Filtering.plots")
+create_set_OutDir(OutDirOrig, "Filtering.plots3D")
+plot3DFiltLinear = F
 
-stopifnot(length(combined.obj$log10.HGA_Markers) > 0 )
+# stopifnot(length(combined.obj$log10.HGA_Markers) > 0 )
+
 
 # Calculate ------------------------
 
-i=3
-plotting.data <- ls.Seurat[[i]]@meta.data
-plotting.data$'percent.ribo' <- clip.outliers(plotting.data$percent.ribo, probs = c(.005, .995))
-plotting.data$'percent.mito' <- clip.outliers(plotting.data$percent.mito, probs = c(.005, .995))
-plotting.data$'nFeature_RNA' <- clip.outliers(plotting.data$nFeature_RNA, probs = c(.005, .995))
-
-plotting.data$'percent.ribo.log10' <- log10(plotting.data$percent.ribo + 0.01)
-plotting.data$'percent.mito.log10' <- log10(plotting.data$percent.mito + 0.01)
-plotting.data$'log_nFeature_RNA' <- log10(plotting.data$nFeature_RNA+1)
+for (i in 1:n.datasets) {
+  iprint(names(ls.Seurat)[i], percentage_formatter(i/n.datasets, digitz = 1))
 
 
-# Plot3D ------------------------
-plt <-  plot_ly(data = plotting.data
-               , x = ~log_nFeature_RNA, y = ~percent.mito.log10, z = ~percent.ribo.log10
-               , type = "scatter3d"
-               , mode = "markers"
-               , marker = list(size = 2)
-               , color = ~log10.HGA_Markers
-               , colors = c('blue', 'red')
-               , opacity = .9
-               #, hoverinfo="text"
-               # , text=~label
-) %>% layout(scene = list(aspectmode = "manual", title="Filtering in 3 parameters separate dying cells"
-                               , aspectratio = list(x=1, y=1, z=1)))
+  plotting.data <- ls.Seurat[[i]]@meta.data
+  plotting.data$'percent.ribo' <- clip.outliers(plotting.data$percent.ribo, probs = c(.005, .995))
+  plotting.data$'percent.mito' <- clip.outliers(plotting.data$percent.mito, probs = c(.005, .995))
+  plotting.data$'nFeature_RNA' <- clip.outliers(plotting.data$nFeature_RNA, probs = c(.005, .995))
 
-plt
-SavePlotlyAsHtml(plt, category. = "Filtering.log10")
+  plotting.data$'percent.ribo.log10' <- log10(plotting.data$percent.ribo + 0.01)
+  plotting.data$'percent.mito.log10' <- log10(plotting.data$percent.mito + 0.01)
+  plotting.data$'log_nFeature_RNA' <- log10(plotting.data$nFeature_RNA + 1)
 
-# Plot3Dlog scale ------------------------
-plt <-  plot_ly(data = plotting.data
-                , x = ~nFeature_RNA, y = ~percent.mito, z = ~percent.ribo
-                , type = "scatter3d"
-                , mode = "markers"
-                , marker = list(size = 2)
-                , color = ~log10.HGA_Markers
-                , colors = c('blue', 'red')
-                , opacity = .9
-                #, hoverinfo="text"
-                # , text=~label
-) %>% layout(scene = list(aspectmode = "manual", title="Filtering in 3 parameters separate dying cells"
-                          , aspectratio = list(x=1, y=1, z=1)))
-plt
-SavePlotlyAsHtml(plt, category. = "Filtering")
+
+  # Plot3D log scale ------------------------
+  plt <-  plot_ly(data = plotting.data
+                 , x = ~log_nFeature_RNA, y = ~percent.mito.log10, z = ~percent.ribo.log10
+                 , type = "scatter3d"
+                 , mode = "markers"
+                 , marker = list(size = 2)
+                 , color = ~log10.HGA_Markers
+                 , colors = c('blue', 'red')
+                 , opacity = .9
+                 #, hoverinfo="text"
+                 # , text=~label
+  ) %>% layout(scene = list(aspectmode = "manual"
+                            , title = "Filtering in 3 parameters separate dying cells"
+                            , aspectratio = list(x = 1, y = 1, z = 1)))
+
+  # plt
+
+  SavePlotlyAsHtml(plt, category. = ppp("log10.Filtering", meta.tags$library[i]))
+
+  # Plot3D ------------------------
+  if (plot3DFiltLinear) {
+    plt <-  plot_ly(data = plotting.data
+                    , x = ~nFeature_RNA, y = ~percent.mito, z = ~percent.ribo
+                    , type = "scatter3d"
+                    , mode = "markers"
+                    , marker = list(size = 2)
+                    , color = ~log10.HGA_Markers
+                    , colors = c('blue', 'red')
+                    , opacity = .9
+                    #, hoverinfo="text"
+                    # , text=~label
+    ) %>% layout(scene = list(aspectmode = "manual", title = "Filtering in 3 parameters separate dying cells"
+                              , aspectratio = list(x = 1, y = 1, z = 1)))
+    # plt
+    SavePlotlyAsHtml(plt, category. = ppp("Filtering", meta.tags$library[i]))
+  }
+
+}
 
 
 
