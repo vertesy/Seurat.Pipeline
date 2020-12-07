149d39e7adfd75f0621093bc92eeaf46edaadccf
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-07 19:18:37 +0100

    downsample works

--- Dowsample.Seurat.Objects.R
+++ Dowsample.Seurat.Objects.R
@@ -4,26 +4,22 @@
 # source("~/GitHub/Packages/Seurat.pipeline/Dowsample.Seurat.Objects.R")
 # rm(list = ls(all.names = TRUE)); try(dev.off(), silent = T)
 
-# Functions ------------------------
-try(source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= F)
-require('MarkdownReportsDev')
-# source('~/Github/TheCorvinas/R/DatabaseLinke.r')
-
-# Setup ------------------------
-OutDir = "~/Dropbox/Abel.IMBA/AnalysisD/"
-setup_MarkdownReports(OutDir = OutDir, scriptname = "")
-OutDirOrig = OutDir
-
 # Metadata ------------------------
-
 # Parameters ------------------------
 
+ls.Seurat.downsampled <- foreach(i=1:n.datasets ) %dopar% {
+  # for (i in 1:n.datasets ) {
+  iprint(names(ls.Seurat)[i], percentage_formatter(i/n.datasets, digitz = 2))
+  sobj <- subsetSeuObj(ls.Seurat[[i]], nCells = p$"dSample.Organoids")
+  # ls.Seurat[[i]] <- sobj
+  sobj
+}; toc();# names(ls.Seurat)  <- samples
+unlapply(ls.Seurat.downsampled, ncol)
+isave.RDS(object = ls.Seurat.downsampled, suffix = ppp(p$"dSample.Organoids", "cells"), inOutDir = T)
 
-# Read In ------------------------
-
-# QC ------------------------
 
 # ------------------------
+unlapply(ls.Seurat, ncol)
 # ------------------------
 # ------------------------
 # ------------------------
