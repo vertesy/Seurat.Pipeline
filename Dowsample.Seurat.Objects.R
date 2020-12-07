ecd7ba92d7548c755021daf4605de9f1016b6405
Abel Vertesy <vertesy@users.noreply.github.com>
2020-12-07 15:44:13 +0100

    init

---
+++ Dowsample.Seurat.Objects.R
@@ -0,0 +1,32 @@
+######################################################################
+# Dowsample.Seurat.Objects.R
+######################################################################
+# source("~/GitHub/Packages/Seurat.pipeline/Dowsample.Seurat.Objects.R")
+# rm(list = ls(all.names = TRUE)); try(dev.off(), silent = T)
+
+# Functions ------------------------
+try(source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R'),silent= F)
+require('MarkdownReportsDev')
+# source('~/Github/TheCorvinas/R/DatabaseLinke.r')
+
+# Setup ------------------------
+OutDir = "~/Dropbox/Abel.IMBA/AnalysisD/"
+setup_MarkdownReports(OutDir = OutDir, scriptname = "")
+OutDirOrig = OutDir
+
+# Metadata ------------------------
+
+# Parameters ------------------------
+
+
+# Read In ------------------------
+
+# QC ------------------------
+
+# ------------------------
+# ------------------------
+# ------------------------
+# ------------------------
+# ------------------------
+
+
