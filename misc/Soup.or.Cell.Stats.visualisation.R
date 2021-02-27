2856bbd5c4c70eaf3afd14cc90a676f0b276fccb
Abel Vertesy <vertesy@users.noreply.github.com>
2021-02-27 14:47:50 +0100

    update to run with HCA

--- misc/Soup.or.Cell.Stats.visualisation.R
+++ misc/Soup.or.Cell.Stats.visualisation.R
@@ -13,17 +13,16 @@ source('~/GitHub/Packages/CodeAndRoll/CodeAndRoll.R')
 try(source("~/GitHub/Packages/ggExpressDev/ggExpress.functions.R"));
 
 # Setup ------------------------------------------------------------------------
-OutDir <- OutDirOrig <- "~/Dropbox/Abel.IMBA/AnalysisD/Abel/SEO/Soup.or.Cell.Stats/SEO/"
+OutDir <- OutDirOrig <- "~/Dropbox/Abel.IMBA/AnalysisD/Abel/SEO/Soup.or.Cell.Stats/HCA/s2/"
 setup_MarkdownReports(OutDir = OutDir, scriptname = 'Soup.or.Cell.Stats.visualisation.R')
-oo()
-
-
-
-
 
 # Read in ------------------------
 
-InputProjectDir = "/Volumes/SNP.deconvolution/abel.202102.vcf/SEO.R10015.124719/"
+# InputProjectDir = "/Volumes/SNP.deconvolution/abel.202102.vcf/SEO.R10015.124719/"
+# InputProjectDir = "/Volumes/SNP.deconvolution/abel.202102.vcf/HCA_Brain_ABhashPool_Bp_TK297s1_transcriptome"
+InputProjectDir = "/Volumes/SNP.deconvolution/abel.202102.vcf/HCA_Brain_ABhashPool_Bp_TK297s2_transcriptome"
+# InputProjectDir = "/Volumes/SNP.deconvolution/abel.202102.vcf/HCA_Brain_ABhashPool_Bp_TK297s3_transcriptome"
+
 path.SubDirs <- list.dirs(path = InputProjectDir)[-1]
 for (i in 1:l(path.SubDirs)) {
 
@@ -47,7 +46,8 @@ for (i in 1:l(path.SubDirs)) {
   nr.lines <- length(cell.lines)
   orig.IDs <- 0:(nr.lines-1)
 
-  file.assignments <- list.files(path = Subdir.path, pattern = "clusters.tsv")
+  file.assignments <- list.files(path = Subdir.path, pattern = "^clusters\\..+tsv|clusters.tsv")
+  stopifnot(length(file.assignments)==1)
   df.assignments <- readr::read_tsv(file = (kpps(Subdir.path,file.assignments)))
 
   # Parse metadata ------------------------
@@ -78,14 +78,16 @@ for (i in 1:l(path.SubDirs)) {
     max1 <- rowMax(df.clean[,1:nr.lines])
     max2 <- apply(df.clean[,1:nr.lines], 1, MaxN, 2)
 
-    max1.clipped <- clip.values(max1, thr = -500, high = F)
+    # max1.clipped <- clip.values(max1, thr = -500, high = F)
+    max1.clipped <- clip.outliers(max1, high = T, probs = 0.05, showhist = T)
+    # range(max1.clipped)
 
     assignment.Q <- tibble(
       "Ratio.2nd.Max" = (-max2 / -max1),
-      "-log.likelihood" = max1.clipped,
+      "log(probability)" = max1.clipped,
         "Line" = df.clean$assignment.named
     )
-    assignment.quality.scatterplots <- qscatter(assignment.Q
+    assignment.quality.scatterplots <- qscatter(assignment.Q, subtitle= "Y axis is cut at the lowest 5% of the values"
                                                 , facet.by = 'Line', ncol = nr.lines
                                                 , cols = rgb(0,.75,0,.5), size = 1,) + geom_density_2d()
     qqSave(assignment.quality.scatterplots, w = nr.lines*4, h = 4, ext = 'jpg')
